d156585b143914ab64539b4c9da8f30b28b8cef9
abdirahmanburyar <buryar313@gmail.com>
2025-05-30 15:58:32 +0300

    galabta jimcaha

--- resources/js/Pages/Transfer/Index.vue
+++ resources/js/Pages/Transfer/Index.vue
@@ -24,30 +24,14 @@
                 <div class="flex items-center justify-between w-full">
                     <!-- Search -->
                     <div class="relative flex-grow max-w-xs">
-                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                fill="currentColor">
-                                <path fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
                         <input type="text" v-model="filters.search"
-                            class="pl-10 pr-4 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+                            class="pl-10 pr-4 py-2 border border-gray-300 w-full focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
                             placeholder="Search a Transfer">
                     </div>
 
                     <!-- Facility Selector -->
                     <div class="relative w-[16%]">
                         <div class="flex items-center">
-                            <div class="absolute left-3 z-10 text-gray-400">
-                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
                             <Multiselect v-model="filters.selected_facility" :options="props.facilities"
                                 :searchable="true" :allow-empty="true" :show-labels="false"
                                 placeholder="All Facilities" label="name" track-by="id"
@@ -63,52 +47,32 @@
 
                     <!-- Warehouse Selector -->
                     <div class="relative w-[16%]">
-                        <div class="flex items-center">
-                            <div class="absolute left-3 z-10 text-gray-400">
-                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                            <Multiselect v-model="filters.selected_warehouses" :options="props.warehouses"
-                                :multiple="true" :close-on-select="false" :clear-on-select="false"
-                                :preserve-search="true" placeholder="All Warehouses" label="name" track-by="id"
-                                class="pl-10 w-full" :preselect-first="false" @input="updateWarehouseFilter">
-                                <template #selection="{ values, search, isOpen }">
-                                    <span class="multiselect__single" v-if="values.length && !isOpen">
-                                        {{ values.length === 1 ? values[0].name : `${values.length} warehouses selected`
-                                        }}
-                                    </span>
-                                </template>
-                            </Multiselect>
-                        </div>
+                        <Multiselect v-model="filters.selected_warehouses" :options="props.warehouses"
+                            :multiple="true" :close-on-select="false" :clear-on-select="false"
+                            :preserve-search="true" placeholder="All Warehouses" label="name" track-by="id"
+                            class="pl-10 w-full" :preselect-first="false" @input="updateWarehouseFilter">
+                            <template #selection="{ values, search, isOpen }">
+                                <span class="multiselect__single" v-if="values.length && !isOpen">
+                                    {{ values.length === 1 ? values[0].name : `${values.length} warehouses selected`
+                                    }}
+                                </span>
+                            </template>
+                        </Multiselect>
                     </div>
 
                     <!-- Location Selector -->
                     <div class="relative w-[16%]">
-                        <div class="flex items-center">
-                            <div class="absolute left-3 z-10 text-gray-400">
-                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                            <Multiselect v-model="filters.selected_locations" :options="props.locations"
-                                :multiple="true" :close-on-select="false" :clear-on-select="false"
-                                :preserve-search="true" placeholder="All Locations" label="location" track-by="id"
-                                class="pl-10 w-full" :preselect-first="false" @input="updateLocationFilter">
-                                <template #selection="{ values, search, isOpen }">
+                        <Multiselect v-model="filters.selected_locations" :options="props.locations"
+                            :multiple="true" :close-on-select="false" :clear-on-select="false"
+                            :preserve-search="true" placeholder="All Locations" label="location" track-by="id"
+                            class="pl-10 w-full" :preselect-first="false" @input="updateLocationFilter">
+                            <template #selection="{ values, search, isOpen }">
                                     <span class="multiselect__single" v-if="values.length && !isOpen">
                                         {{ values.length === 1 ? values[0].location : `${values.length} locations
                                         selected` }}
                                     </span>
                                 </template>
                             </Multiselect>
-                        </div>
                     </div>
 
                     <!-- Date Range Selector -->
