a9481d01ec5eac833a05d252253e602323504835
abdirahmanburyar <buryar313@gmail.com>
2025-06-25 11:07:34 +0300

    transfer dispatch info

--- resources/js/Pages/Transfer/Show.vue
+++ resources/js/Pages/Transfer/Show.vue
@@ -2663,7 +2663,7 @@ async function createDispatch() {
                 icon: "success",
                 confirmButtonText: "OK",
             }).then(() => {
-                router.get(route("transfer.show", props.transfer?.id));
+                router.get(route("transfers.show", props.transfer?.id));
             });
         })
         .catch((error) => {
